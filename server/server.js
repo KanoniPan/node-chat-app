2eb4dc45b70f0c0fb7e93b9709877e42c095a378
KanoniPan <panevschi.ruslan@gmail.com>
2018-02-08 13:29:18 +0200

    bug fix

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const publicPath = path.join(__dirname, '../public');
 // console.log(publicPath);
 
 var app = express();
-const port = process.env.PATH || 3000;
+const port = process.env.PORT || 3000;
 // app.get('/',function(req,res){
 //
 //      res.sendFile(publicPath + '/index.html');

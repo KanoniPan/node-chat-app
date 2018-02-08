a6a1f78c337f832e7f7648bcab9208a28e464e56
KanoniPan <panevschi.ruslan@gmail.com>
2018-02-08 13:16:06 +0200

    first push

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+// console.log(__dirname + '/../public'); or like this(way slower)
+// console.log(publicPath);
+
+var app = express();
+const port = process.env.PATH || 3000;
+// app.get('/',function(req,res){
+//
+//      res.sendFile(publicPath + '/index.html');
+//
+// }); or use like this
+app.use(express.static(publicPath));
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};

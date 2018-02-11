a293ce544a8f8cb302ac34e2548e324ff0a5f113
KanoniPan <panevschi.ruslan@gmail.com>
2018-02-11 19:21:59 +0200

    check

--- server/server.js
+++ server/server.js
@@ -4,25 +4,29 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
-// console.log(__dirname + '/../public'); or like this(way slower)
-// console.log(publicPath);
-
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-const port = process.env.PORT || 3000;
-// app.get('/',function(req,res){
-//
-//      res.sendFile(publicPath + '/index.html');
-//
-// }); or use like this
+
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log("New user connected");
+  console.log('New user connected');
+
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
+
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room name are required.');
+    }
+
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
@@ -32,14 +36,13 @@ io.on('connection', (socket) => {
 
   socket.on('createLocationMessage', (coords) => {
     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
-  })
+  });
 
   socket.on('disconnect', () => {
-    console.log("User was disconnected")
+    console.log('User was disconnected');
   });
 });
+
 server.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
-//
-// module.exports = {app};
